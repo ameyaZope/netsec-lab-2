74d41a010a20d468a5575cd49fc215b01f12490b
ameyaZope <zopeameya@gmail.com>
2024-02-16 14:18:57 -0500

    Adding HTTP packet filter and corresponding output string

--- mysniffer.py
+++ mysniffer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+from time import localtime, strftime
+
 from scapy.all import * # do not import from scapy.util, that rdpcap version has bugs
+from scapy.layers.http import HTTPRequest, HTTP
+from scapy.layers.inet import TCP, IP
 
 
 class PacketSniffer():
@@ -10,17 +14,21 @@ class PacketSniffer():
         self.interface = interface
         self.pcap_file = pcap_file
         self.expression = expression
+        conf.resolve = []
 
     def read_pcap_file(self):
-        print("Reading file name : " + self.pcap_file[0])
         packet_list = sniff(offline=self.pcap_file[0], filter=self.bpf_filter)
-        print(packet_list)
+        for pkt in packet_list:
+            if pkt.haslayer(HTTPRequest):
+                print(f'{strftime("%Y-%m-%d %H:%M:%S", localtime(pkt.time))}.{pkt.time - int(pkt.time)} {pkt[IP].src}.{pkt[TCP].sport} -> {pkt[IP].dst}{pkt[TCP].dport} {pkt[HTTP].Host.decode("UTF-8")} {pkt[HTTP].Method.decode("UTF-8")} {pkt[HTTP].Path.decode("UTF-8")}')
 
     def prepare_expression(self):
         self.bpf_filter = ' '.join(self.expression)
 
     def sniff_interface(self):
         print("Sniffing interface " + self.interface)
+        packet_list = sniff(iface=self.interface, filter=self.bpf_filter)
+        print(packet_list[TCP].dst)
 
     def start(self):
         self.prepare_expression()
@@ -42,6 +50,5 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--read', dest='pcap_file', nargs=1, type=str, action='store', default=None, help='Specify the pcap file to read the packets from')
     parser.add_argument("expression", nargs='*', type=str, action="store", default=None, help='This is the bpf filter that will be applied to the packets captured')
     args = parser.parse_args()
-    print(args)
     pktSniffer = PacketSniffer(args.interface, args.pcap_file, args.expression)
     pktSniffer.start()
