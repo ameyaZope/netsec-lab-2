883af4ab6e68c8eb91e8df0dd93dea7aa0ae6638
ameyaZope <zopeameya@gmail.com>
2024-03-08 15:51:10 -0500

    Removing unnecessary changes for TLSv1.3

--- mysniffer.py
+++ mysniffer.py
@@ -10,6 +10,7 @@ from scapy.layers.tls.all import TLS, ServerName
 from scapy.layers.tls.extensions import TLS_Ext_SupportedVersion_SH, TLS_Ext_SupportedVersions, \
     TLS_Ext_SupportedVersion_CH
 from scapy.layers.tls.handshake import TLSClientHello, TLS13ClientHello
+import re
 
 
 def display_single_packet(pkt):
@@ -34,13 +35,6 @@ def display_single_packet(pkt):
             print()
             print(
                 f'{strftime("%Y-%m-%d %H:%M:%S", localtime(pkt.time))}.{pkt.time - int(pkt.time)} HTTP {pkt[IP].src}:{pkt[TCP].sport} -> {pkt[IP].dst}:{pkt[TCP].dport} {pkt[HTTP].Host.decode()} {pkt[HTTP].Method.decode()} {pkt[HTTP].Path.decode()}')
-        elif pkt.haslayer(TLSClientHello) and pkt[TLSClientHello].version==771 and pkt.haslayer(TLS_Ext_SupportedVersion_CH) and 772 in pkt[TLS_Ext_SupportedVersion_CH].versions:
-            if pkt.haslayer(ServerName):
-                print(
-                    f'{strftime("%Y-%m-%d %H:%M:%S", localtime(pkt.time))}.{pkt.time - int(pkt.time)} TLS 1.3 {pkt[IP].src}:{pkt[TCP].sport} -> {pkt[IP].dst}:{pkt[TCP].dport} {pkt[ServerName].servername.decode()}')
-            else:
-                print(
-                    f'{strftime("%Y-%m-%d %H:%M:%S", localtime(pkt.time))}.{pkt.time - int(pkt.time)} TLS 1.3 {pkt[IP].src}:{pkt[TCP].sport} -> {pkt[IP].dst}:{pkt[TCP].dport}')
         elif pkt.haslayer(TLSClientHello):
             if pkt.haslayer(ServerName):
                 print(
